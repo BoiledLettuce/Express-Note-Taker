a2f2634e218ee50d623052e7df6a1daad60ea990
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-02 21:27:45 -0700

    more code to server.js

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlROutes")(app);
+
+app.listen(PORT, function() {
+    console.log("PORT listened" + PORT);
+});
