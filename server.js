430f4a9914efd2ff59b841d3d6f4070af5cbcc19
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-08 15:27:32 -0700

    fixed bug at line 12

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true })); //DATA PARSING
 app.use(express.static('public'));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app); //ROUTES
-require("./routes/htmlROutes")(app);
+require("/routes/apiRoutes")(app); //ROUTES
+require("/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() { //SERVER START
     console.log("Server started @ PORT " + PORT);
