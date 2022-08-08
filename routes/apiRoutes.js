e7bc3fb9d60dbc2cfd4cc37e4de6574567da6b30
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-08 14:16:03 -0700

    fixed dependencies, added ability to delete notes, added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,64 @@
 const fs = require('fs');
+
+module.exports = function (app) { //GET
+    app.get("/api/notes", function (req, res) {
+        console.log("server activity");
+        fs.readFile("db/db.json", "utf-8", (err, data) => {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            res.json(notes);
+        });
+    });
+
+    app.post("/api/notes", function (req, res) { //POST
+        fs.readFile("db/db.json", "utf-8", (err, data) => {
+            if (err) throw err;
+
+            let notes = JSON.parse(data);
+            let newNotes = req.body;
+            let newId = (notes.length).toString();
+            newNotes.id = newId;
+            notes.push(newNotes);
+
+            fs.writeFileSync("db/db.json", JSON.stringify(notes), "utf-8", (err, data) => {
+                if (err) throw err;
+                console.log(`it worked ¯\_(ツ)_/¯`); 
+            });
+            res.json(notes);
+        });
+    });
+
+    app.delete("/api/notes/:id", function (req, res) { //DELETE (╯°□°）╯︵ ┻━┻
+        fs.readFile("db/db.json", "utf-8", (err, data) => {
+            if (err) throw err;
+            let note = JSON.parse(data);
+            let noteId = req.params.id;
+            let newNoteId = 0;
+
+            note = note.filter(currNotes => {
+                return currNotes.id != noteId;
+            });
+            for (currNotes of note) {
+                currNotes.id = newNoteId.toString();
+                newNoteId++;
+            }
+            fs.writeFileSync("./db/db.json", JSON.stringify(note), "utf-8", (err, data) => {
+                if (err) throw err;
+                console.log(`dear lord have mercy ¯\_(⊙︿⊙)_/¯ `)
+            });
+
+            res.json(note);
+        });
+
+        
+    });
+
+
+
+
+
+
+
+
+
+}
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.urlencoded({ extended: true })); //DATA PARSING
 app.use(express.static('public'));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app); //ROUTES
 require("./routes/htmlROutes")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function() { //SERVER START
     console.log("PORT listened" + PORT);
 });
