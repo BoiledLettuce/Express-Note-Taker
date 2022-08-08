25b27cc80c2ada8066e07ee72124d2d0fbd32b89
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-08 16:27:35 -0700

    fixed procfile and port

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express'); //DEPENDENCY
 const path = require('path');
 
 const app = express(); //EXPRESS
-const PORT = 8080; //3000 or 8080
+// const PORT = 8080; //3000 or 8080
+const PORT = process.env.PORT || 5000
 
 app.use(express.urlencoded({ extended: true })); //DATA PARSING
 app.use(express.static('public'));
